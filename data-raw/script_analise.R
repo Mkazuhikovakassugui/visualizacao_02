8b63eb1cb1a981a10868c793a490179803a04030
Mkazuhikovakassugui <marcio.kazuhiko.vakassugui@gmail.com>
2022-06-30 11:24:58 -0300

    inseri os códigos para a criação dos gráficos 2 e 3.

--- data-raw/script_analise.R
+++ data-raw/script_analise.R
@@ -1,21 +1,27 @@
-#------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------#
 #                                        Análise da base ENEM
 #                                    Curso- R - Visualização II
 #                                     Professora: Beatriz Milz
 #                                     Aluno: Marcio Vakassugui
-#------------------------------------------------------------------------------------------------------------#
+#---------------------------------------------------------------------------------------------#
 
 
-# Carregamento da base arrumada -------------------------------------------------------------------------
+# Carregamento da base arrumada ---------------------------------------------------------------
 
-enem <- readr::read_csv("data/enem_analise.csv", show_col_types = FALSE)
+enem <- readr::read_csv("data/enem_analise_final.csv", show_col_types = FALSE)
+
+# Carregamento das funções
+source("R/funcao_tema_graficos.R")
+
+enem_cidade <- enem |>
+  dplyr::filter(nu_ano == 2020, no_municipio_prova == "Santa Helena")
 
 
-# ANO DE 2018
-# ANÁLISE DAS 5 COMPETÊNCIAS DA REDAÇÃO DO ENEM ---------------------------------------------------------
+# ANO DE 2020
+# ANÁLISE DAS 5 COMPETÊNCIAS DA REDAÇÃO DO ENEM -----------------------------------------------
 
-# A nota da redação do ENEM é composta por 5 competências. Cada competência pode receber notas de 0 a 200
-# pontos. A soma das competências resulta na nota da Nota da Redação.
+# A nota da redação do ENEM é composta por 5 competências. Cada competência pode receceber
+# notas de 0 a 200 pontos. A soma das competências resulta na nota da Nota da Redação.
 
 # ANÁLISE DAS COMPETÊNCIAS
 # São as competências que compõem as notas da redação do ENEM
@@ -27,14 +33,13 @@ enem <- readr::read_csv("data/enem_analise.csv", show_col_types = FALSE)
 # ANÁLISES DAS MÉDIAS DAS NOTAS QUE COMPÕEM AS NOTAS DA REDAÇÃO
 
 # Cálculo das médias dos componentes da redação
-medias_comp_redacao <- enem |>
-  dplyr::filter(nu_ano == 2018, no_municipio_prova == "Curitiba") |>
+medias_comp_redacao <- enem_cidade |>
   dplyr::summarise(dplyr::across(
     .cols = dplyr::starts_with("nu_nota_comp"),
     .fns = mean, na.rm = TRUE
   ))
 
-medias_comp_redacao <- medias_comp_redacao |>                                          # renomear as variáveis
+medias_comp_redacao <- medias_comp_redacao |>                           # renomear as variáveis
   dplyr::rename(
     "comp1" = "nu_nota_comp1",
     "comp2" = "nu_nota_comp2",
@@ -42,7 +47,6 @@ medias_comp_redacao <- medias_comp_redacao |>                                   
     "comp4" = "nu_nota_comp4",
     "comp5" = "nu_nota_comp5"
   )
-
 
 # VISUALIZAÇÃO DO RESULTADO
 
@@ -60,7 +64,7 @@ medias_comp_redacao <- medias_comp_redacao |>
     dplyr::mutate(notas_comp_redacao = as.factor(notas_comp_redacao))
 
 # Cosntruir o gráfico de barras das médias dos componentes que compõem a nota da redação
-p1 <- medias_comp_redacao |>                                            # base
+p1 <- medias_comp_redacao |>                                                             # base
   dplyr::mutate(
     notas_comp_redacao = forcats::fct_reorder(
       notas_comp_redacao,
@@ -68,7 +72,7 @@ p1 <- medias_comp_redacao |>                                            # base
       .desc = TRUE
     )
   ) |>
-  ggplot2::ggplot() +                                                   # gráfico
+  ggplot2::ggplot() +                                                                 # gráfico
   ggplot2::aes(
     x = notas_comp_redacao,
     y = medias,
@@ -77,78 +81,138 @@ p1 <- medias_comp_redacao |>                                            # base
   ggplot2::geom_col(
     fill = "#576BC7"
   ) +
-  ggplot2::geom_label(                                                  # labels
+  ggplot2::geom_label(                                                                 # labels
     size = 4,
     color = "#D6F3FF",
     alpha = 0,
     nudge_y = -10
   ) +
-  ggplot2::labs(                                                        # títulos dos eixos
+  ggplot2::labs(                                                            # títulos dos eixos
     title = "Médias dos componentes da redação",
     subtitle = "A nota da redação é definida pela soma dos componentes",
     x = "notas dos componentes",
     y = "média das notas"
   ) +
-  ggplot2::theme_classic() +                                            # tema
+  ggplot2::theme_classic() +                                                             # tema
   ggplot2::theme(
     legend.position = "none"
   ) +
-  ggplot2::theme(                                                       # customização do tema
-    panel.background = ggplot2::element_rect(fill = "#D6F3FF"),                # cor do fundo do gráfico
-    plot.background = ggplot2::element_rect(fill = "#D6F3FF"),                 # cor da grade externa
-    plot.margin = ggplot2::unit(c(1, 2, 1, 1), "cm"),                          # distância das margens
-    plot.title = ggplot2::element_text(                                        # título do gráfico.
-      size = 18,                                                                     # tamanho da fonte
-      face = "bold",                                                                # não negrito
-      family = "",                                                                   # fonte do título
-      hjust = 0,                                                                     # posição na horizontal
-      margin = ggplot2::unit(c(0, 0, 0.5, 0.5), "cm")                                # margens do título
-    ),
-    plot.subtitle = ggplot2::element_text(                                     # subtítulo
-      size = 11,                                                                     # tamanho da fonte
-      family = "",                                                                   # fonte do subtítulo
-      hjust = 0,                                                                     # posição na horizontl
-    ),
-    text = ggplot2::element_text(                                              # textos config. geral de texto
-      family = "",                                                                   # fonte
-      color = "#000E14",                                                             # cor
-      size = 13,                                                                     # tamanho da fonte
-      hjust = 0,                                                                     # posição na horizontal
-      face = "bold"                                                                  # negrito
-    ),
-    axis.title = ggplot2::element_text(                                        # texto do título do eixo x e y
-        face = "bold",                                                                 # negrito
-        size = 13,                                                                     # tamanho
-        hjust = 0.5,                                                                   # posição na horizontal
-    ),
-    axis.text.x = ggplot2::element_text(                                       # texto do eixo x
-      color = "#000E14",                                                             # cor
-      size = 11,                                                                    # tamanho
-      face = "plain",                                                                 # negrito
-      margin = ggplot2::unit(c(0.3, 0, 0.5, 0), "cm")                                # margens do texto
-    ),
-    axis.text.y = ggplot2::element_text(                                       # texto do eixo y
-      color = "#000E14",                                                             # cor
-      size = 11,                                                                    # tamanho
-      face = "plain",                                                                 # negrito
-      family = "",                                                                   # fonte
-      margin = ggplot2::unit(c(0, 0.5, 0, 0.5), "cm")                                # margens do texto
-    ),
-    axis.ticks.x = ggplot2::element_line(color = "#1F1F1F"),                   # ticks do eixo x
-    axis.line.x = ggplot2::element_line(color = "#1F1F1F"),                    # cor da linha do eixo x
-    axis.ticks.y = ggplot2::element_line(color = "#1F1F1F"),                   # padrões dos ticks do eixo x
-    axis.line.y = ggplot2::element_line(color = "#1F1F1F"),                    # cor da linha do eixo y
-  )
-
+  theme_enem()
 p1
 
-
-medias_comp_redacao |>
+max_media <- medias_comp_redacao |>
     dplyr::slice_max(order_by = medias)
 
-medias_comp_redacao |>
+min_media <- medias_comp_redacao |>
     dplyr::slice_min(order_by = medias)
 
+
+# CONSTRUIR GRÁFICO DE BOXPLOT  - UM BOXPLOT PARA CADA COMPETÊNCIA
+
+# pivotar a base
+comp_redacao <- enem_cidade |>
+  dplyr::select(nu_nota_comp1,
+                nu_nota_comp2,
+                nu_nota_comp3,
+                nu_nota_comp4,
+                nu_nota_comp5) |>
+  dplyr::rename("comp1" = "nu_nota_comp1",
+                "comp2" = "nu_nota_comp2",
+                "comp3" = "nu_nota_comp3",
+                "comp4" = "nu_nota_comp4",
+                "comp5" = "nu_nota_comp5",)
+
+comp_redacao_long <- comp_redacao |>
+  tidyr::pivot_longer(
+    cols = c(
+      "comp1",
+      "comp2",
+      "comp3",
+      "comp4",
+      "comp5"
+    ),
+    names_to = "competencias",
+    values_to = "notas_comp"
+  ) |>
+  dplyr::mutate(competencias = as.factor(competencias))
+
+p2 <- comp_redacao_long |>
+  ggplot2::ggplot(ggplot2::aes(competencias, notas_comp, fill = competencias))+
+  ggplot2::geom_violin(width = 0.6)+
+  ggplot2::geom_boxplot(width = 0.1, color = "black", alpha = 0.2)+
+  ggplot2::geom_jitter(color="black", size=0.4, alpha=0.05) +
+  ggplot2::labs(
+    title = "Violin e boxplot das competências",
+    x = "Competências",
+    y = "notas"
+  )+
+  theme_enem()+
+  ggplot2::theme(
+    legend.position = "none"
+  )
+
+
+p2
+
+
+# BOXPLOT COMPETENCIAS E NOTAS -----------------------------------------------------------
+
+enem_cidade |>
+  dplyr::select(nu_nota_comp1, )
+  ggplot2::ggplot()+
+  aes(x = )
+
+
+
+# GRÁFICO DE COLUNAS - INTERVALO DE NOTAS X FREQUENCIAS RELATIVAS ------------------------
+
+fabs_notas_redacao <- enem_cidade |>
+  dplyr::select(nu_nota_redacao) |>
+  table()
+
+# total de alunos com nota máxima
+
+qtd_nota_max <- fabs_notas_redacao |>
+  tail(n=1)
+
+# criar as classes de frequências das notas
+
+intervalo_classes_notas <- seq(0, 1000, 100)
+tab_classes_notas <- table(cut(enem_cidade$nu_nota_redacao,
+                               breaks = intervalo_classes_notas,
+                               right = FALSE)) |>
+  prop.table() |>
+  data.frame() |>
+  dplyr::mutate(Freq = Freq*100)
+
+
+p2 <- tab_classes_notas |>
+  ggplot2::ggplot()+
+  ggplot2::aes(x= Var1,
+               y = Freq,
+               label = round(Freq, 3))+
+  ggplot2::geom_col(fill = "#576BC7") +
+  ggplot2::geom_label(                                                                 # labels
+    size = 4,
+    color = "#576BC7",
+    alpha = 0,
+    nudge_y = 1
+  ) +
+  ggplot2::labs(                                                            # títulos dos eixos
+    title = "Frequências relativas de notas por intervalos de classe",
+    x = "Intervalo de notas",
+    y = "Frequência (%)"
+  )+
+  ggplot2::theme_classic()+
+  theme_enem()
+
+p2
+
+
+
+
+
+´# Notas médias nas edições de 2018, 2019 e 2020 ------------------------------------------
 
 
 
