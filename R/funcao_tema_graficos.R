e591754eaa3ce532344a8cdbc76ed89645324364
Mkazuhikovakassugui <marcio.kazuhiko.vakassugui@gmail.com>
2022-07-03 23:13:44 -0300

    criado opção para tema de gráfico com fundo do gráfico branco.

--- R/funcao_tema_graficos.R
+++ R/funcao_tema_graficos.R
@@ -47,3 +47,54 @@ theme_enem <- function(variables) {
     )
 
 }
+
+
+theme_enem_fundo_branco <- function(variables) {
+    ggplot2::theme(                                                      # customização do tema
+        panel.background = ggplot2::element_rect(fill = "#FFFFFF"),
+        plot.background = ggplot2::element_rect(fill = "#D6F3FF"),
+        plot.margin = ggplot2::unit(c(1, 2, 1, 1), "cm"),                              # título
+        plot.title = ggplot2::element_text(
+            size = 24,
+            face = "bold",
+            family = "",
+            hjust = 0,
+            margin = ggplot2::unit(c(0, 0, 0.5, 0.5), "cm")
+        ),
+        plot.subtitle = ggplot2::element_text(                                      # subtítulo
+            size = 15,
+            family = "",
+            hjust = 0,
+        ),
+        text = ggplot2::element_text(                              # textos config. geral texto
+            family = "",
+            color = "#000E14",
+            size = 14,
+            hjust = 0,
+            face = "bold"
+        ),
+        axis.title = ggplot2::element_text(                         # texto do título eixo x/y
+            face = "bold",
+            size = 16,
+            hjust = 0.5,
+        ),
+        axis.text.x = ggplot2::element_text(                                  # texto do eixo x
+            color = "#000E14",
+            size = 16,
+            face = "plain",
+            margin = ggplot2::unit(c(0.3, 0, 0.5, 0), "cm")
+        ),
+        axis.text.y = ggplot2::element_text(                                  # texto do eixo y
+            color = "#000E14",
+            size = 16,
+            face = "plain",
+            family = "",
+            margin = ggplot2::unit(c(0, 0.5, 0, 0.5), "cm")
+        ),
+        axis.ticks.x = ggplot2::element_line(color = "#1F1F1F"),              # ticks do eixo x
+        axis.line.x = ggplot2::element_line(color = "#1F1F1F"),        # cor da linha do eixo x
+        axis.ticks.y = ggplot2::element_line(color = "#1F1F1F"),     # padrões dos ticks eixo x
+        axis.line.y = ggplot2::element_line(color = "#1F1F1F"),        # cor da linha do eixo y
+    )
+
+}
